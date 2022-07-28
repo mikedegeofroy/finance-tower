44925d21aa41dcc54a185cf2b7086886f543caac
Mike de Geofroy <59496187+MikeDeGeofroy@users.noreply.github.com>
2022-07-28 14:43:34 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent:"#006fcf",
+      },
+    }
+  },
+  plugins: [],
+}
